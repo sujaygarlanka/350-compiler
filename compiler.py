ab2b556b64d706f3ff18805e71d317578c485366
Sujay Garlanka <sujay.garlanka@gmail.com>
2018-03-01 15:49:45 -0500

    hello

--- compiler.py
+++ compiler.py
@@ -6,7 +6,7 @@ instructions = [x.strip() for x in instructions]
 file1.close()
 
 
-outputFile = open('mem1.mif','w')
+outputFile = open('pc1.mif','w')
 newLine = lambda: outputFile.write('\n')
 outputFile.write('WIDTH=32;')
 newLine()
@@ -147,7 +147,7 @@ for instrLine in instructions:
         line += opcode[instr[0]]
         line += str(binary_repr(int(instr[1]),27))
         
-    elif instr[0] =='setx:
+    elif instr[0] =='setx':
         line += opcode[instr[0]]
         line += str(binary_repr(int(instr[1]),27))
         
