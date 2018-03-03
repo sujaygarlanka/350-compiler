d01228da22a24244cf4e98dc321e11375de0c3bc
Sujay Garlanka <sujay.garlanka@gmail.com>
2018-03-03 16:34:33 -0500

    sw and branching not working

--- processor_tb_base.v
+++ processor_tb_base.v
@@ -8,48 +8,41 @@ module processor_tb_auto();
 
 	integer CYCLE_LIMIT = CYCLE_LIMIT_AUTO_GENERATE; // Modify this to change number of cycles run during test
 
-	reg clock = 0, reset = 0;
+	reg clock = 0, reset = 1;
 	integer cycle_count = 0, error_count = 0;
 	
 	// Probes
-	// wire [31:0] instruction = dut.my_processor.fetch.instruction_out;
-	// wire [31:0] r0 = dut.my_regfile.register_output[0];
-	// wire [31:0] r1 = dut.my_regfile.register_output[1];
-	// wire [31:0] r2 = dut.my_regfile.register_output[2];
-	// wire [4:0] alu_op = dut.my_processor.execute.ctrl_alu_op;
-	// wire [31:0] alu_a = dut.my_processor.execute.operand_A;
-	// wire [31:0] alu_b = dut.my_processor.execute.operand_B;
-	// wire [31:0] rd_data = dut.my_processor.writeback.rf_write_data;
-	// wire rd_enable = dut.my_processor.writeback.rf_write_enable;
-	// wire [4:0] rd_ctrl = dut.my_processor.writeback.rf_write_ctrl;
-	// wire [4:0] alu_shift = dut.my_processor.execute.math_unit.ctrl_shiftamt;
-	// wire [4:0] regfile_ctrlA = dut.my_processor.ctrl_readRegA;
-	// wire [4:0] regfile_ctrlB = dut.my_processor.ctrl_readRegB;
-	// wire [4:0] decode_ctrl_b = dut.my_processor.ctrl_readRegB;
-	// wire [31:0] q_dmem = dut.my_processor.q_dmem;
-
-	// wire [31:0] pc_pc_in = dut.my_processor.latch_pc_pc_in;
-	// wire [31:0] pc_pc_out = dut.my_processor.latch_pc_pc_out;
-	// wire [31:0] decode_a_out = dut.my_processor.decode.a_out;
-	// wire [31:0] decode_b_out = dut.my_processor.decode.b_out;
-	// wire [31:0] execute_a_in = dut.my_processor.execute.a_in;
-	// wire [31:0] execute_b_in = dut.my_processor.execute.b_in;
-	// wire [31:0] execute_o_out = dut.my_processor.execute.o_out;
-	// wire [31:0] execute_b_out = dut.my_processor.execute.b_out;
-	// wire [31:0] memory_o_in = dut.my_processor.memory.o_in;
-	// wire [31:0] memory_b_in = dut.my_processor.memory.b_in;
-	// wire [31:0] memory_o_out = dut.my_processor.memory.o_out;
-	// wire [31:0] memory_d_out = dut.my_processor.memory.d_out;
-	// wire [31:0] writeback_o_in = dut.my_processor.writeback.o_in;
-	// wire [31:0] writeback_d_in = dut.my_processor.writeback.d_in;
+	wire [31:0] pc = dut.my_processor.pc_out;
+	wire [31:0] mem_data_in = dut.my_processor.mem_data_in;
+	wire [31:0] data = dut.my_processor.data;
+	wire [31:0] memData = dut.my_processor.memData;
+	wire [31:0] q_imem = dut.my_processor.q_imem;
+	wire [31:0] q_dmem = dut.my_processor.q_dmem;
+	wire [31:0] address_dmem = dut.my_processor.address_dmem;
+	wire [4:0] ctrl_writeReg = dut.ctrl_writeReg;
+	wire [31:0] data_writeReg = dut.data_writeReg;
+	wire [31:0] writeback_instr = dut.my_processor.write.q_imem;
+	wire [31:0] data_readRegA = dut.data_readRegA;
+	wire [31:0] data_readRegB = dut.data_readRegB;
+	wire ctrl_memToReg = dut.my_processor.write.ctrl_memToReg;
+	wire [31:0] alu_calc = dut.my_processor.my_alu.data_out;
+	wire [31:0] alu_shamt = dut.my_processor.my_alu.shamt;
+	wire [31:0] alu_regA = dut.my_processor.my_alu.regA;
+	wire [31:0] alu_regB = dut.my_processor.my_alu.regB;
+	wire [31:0] alu_opB = dut.my_processor.my_alu.operandBIn;
+	wire [31:0] alu_imm = dut.my_processor.my_alu.extendedImmediate;
+	wire [16:0] alu_wimm = dut.my_processor.my_alu.q_imem[16:0];
+	wire [31:0] decode_latch_in = dut.my_processor.instr_decode_in;
+	wire [31:0] decode_latch_out = dut.my_processor.instr_execute_in;
 	
 	// DUT 
 	skeleton dut(clock, reset);
 	
 	// Main: wait specified cycles, then perform tests
 	initial begin
-		$display($time, ":  << Starting Test >>");	
-		#(20*(CYCLE_LIMIT+1.5))
+		$display($time, ":  << Starting Test >>");
+		#12 reset = 0;	
+		#(20*(CYCLE_LIMIT+1.5));
 		performTests();		
 		$display($time, ":  << Test Complete >>");
 		$display("Errors: %d" , error_count);
@@ -62,7 +55,14 @@ module processor_tb_auto();
 	end
 	
 	always begin
-		#20   cycle_count = cycle_count + 1;
+		@(posedge clock);
+		if (reset != 1) begin
+			cycle_count = cycle_count + 1;
+		end
+	end
+
+	always begin
+		#10 $display("Current Cycle: %d. Current PC: %b",cycle_count, pc[11:0]); // toggle every half-cycles
 	end
 	
 	task checkRegister; // Note: this assumes regfile works properly and has a 2D array "register_output" that  holds all register values
